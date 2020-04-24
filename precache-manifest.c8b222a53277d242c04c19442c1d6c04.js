fd2db1faaab5dd780f940acf9068e4dbe4a8f7f4
vaidik77 <50943011+vaidik77@users.noreply.github.com>
2020-04-24 18:47:52 +0530

    Updates

---
+++ precache-manifest.c8b222a53277d242c04c19442c1d6c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fde955ff2b21c304b08cfb8c62ec962",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "4b8cf727827c573f2b78",
+    "url": "/monsters/static/css/main.db09a64f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8cf727827c573f2b78",
+    "url": "/monsters/static/js/main.da459cbb.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
